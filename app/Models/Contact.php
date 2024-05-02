7f3f42c8fe7a4eda1528bfe681f4ac09df3ddfb0
Ferdy Herdianto <ferdyherdianto949@gmail.com>
2024-05-02 09:48:17 +0700

    membuat Address Model & membuat relasi Address Contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
@@ -14,8 +15,16 @@ class Contact extends Model
     public $incrementing = true;
     public $timestamps = true;
 
+    // Relasi Contact User
+    // 1 contact dimiliki 1 user
     public function user(): BelongsTo
     {
         return $this->belongsTo(Contact::class, "user_id", "id");
+    }
+
+    // 1 contact memiliki N address
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class, "contact_id", "id");
     }
 }
