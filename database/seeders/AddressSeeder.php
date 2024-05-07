c780c705db8cd84cd5b4b0a30efba107fdc12a62
Ferdy Herdianto <ferdyherdianto949@gmail.com>
2024-05-07 13:12:28 +0700

    membuat Get Address API

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\Contact;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,14 @@ class AddressSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $contact = Contact::query()->limit(1)->first();
+        Address::create([
+            'contact_id' => $contact->id,
+            'street' => 'Street Test',
+            'city' => 'City Test',
+            'province' => 'Province Test',
+            'country' => 'Country Test',
+            'postal_code' => '123123'
+        ]);
     }
 }
